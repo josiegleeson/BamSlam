d1dab3f757a27a31afd615bdaef8f0f3f7022539
josiegleeson <30969357+josiegleeson@users.noreply.github.com>
2020-11-27 10:12:45 +1100

    Update BamSlam.R

--- BamSlam.R
+++ BamSlam.R
@@ -95,7 +95,10 @@ main <- function() {
   
   bam_data <- bam_data %>% 
     dplyr::mutate(coverage=width/txLengths.tx_len)
-    tidyr::drop_na(coverage)
+
+  bam_data$coverage <- as.numeric(bam_data$coverage)
+  bam_data <- bam_data %>% 
+    tidyr::drop_na(coverage)           
   
   # Filter 
   bam_data <- bam_data %>% 
